d4c44489e191f068bfe607890f1a4351defbcee5
XPC <XPC@DESKTOP-GH2R1UB>
2024-12-02 16:10:07 -0600

    finalizacion semana 11

---
+++ src/main/java/com/tienda/service/RegistroService.java
@@ -0,0 +1,19 @@
+package com.tienda.service;
+
+import com.tienda.domain.Usuario;
+import jakarta.mail.MessagingException;
+import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface RegistroService {
+    
+    //pre-registro
+    public Model activar(Model model, String usuario, String clave);
+
+    public Model crearUsuario(Model model, Usuario usuario) throws MessagingException;
+    
+    //completar el proceso de activacion
+    public void activar(Usuario usuario, MultipartFile imagenFile);
+    
+    public Model recordarUsuario(Model model, Usuario usuario) throws MessagingException;
+}
